9b59c2d2927bb0e90c6a7ea22c019ee9ec4f78b7
oleg <cyganenkooleg37@gmail.com>
2023-11-16 19:12:48 +0200

    converter from cm to unches

--- test_library.py
+++ test_library.py
@@ -1,12 +1,18 @@
 import pytest
 
 import library
+
+
 def test_convereter():
-   result = library.converter(-2.54)
-   assert result
+    result = library.converter(-2.54)
+    assert result
+
+
 def test_convereter_1():
-   result = library.converter(-2.54)
-   assert result
+    result = library.converter(-2.54)
+    assert result
+
+
 def test_convereter_2():
-   with pytest.raises(TypeError):
-      library.converter("xdddd")
+    with pytest.raises(TypeError):
+        library.converter("xdddd")
